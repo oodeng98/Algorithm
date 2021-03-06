b295bd79766bb98859a956c6edd9d955a94afb0e
oodeng98 <52957193+oodeng98@users.noreply.github.com>
2021-03-06 21:18:10 +0900

    풀이 과정
    git 사용 전이라 풀이과정 생략

---
+++ 11021 A+B - 7/16348326 20191203 맞았습니다!! 1112KB 0ms.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int n;
+	int a,b;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++){
+		scanf("%d",&a);
+		scanf("%d",&b);
+		printf("Case #%d: %d\n",i+1,a+b);
+	}
+	return 0;
+}
