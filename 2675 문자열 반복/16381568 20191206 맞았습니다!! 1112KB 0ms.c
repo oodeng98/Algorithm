8aef0bfac0ff5afbbdd184a58af7a056191dc601
oodeng98 <52957193+oodeng98@users.noreply.github.com>
2021-03-07 00:34:17 +0900

    풀이 과정
    git 사용 전이라 풀이과정 생략

---
+++ 2675 문자열 반복/16381568 20191206 맞았습니다!! 1112KB 0ms.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+	int t;
+	scanf("%d",&t);
+	for(int i=0;i<t;i++){
+		int num;
+		scanf("%d",&num);
+		char word[21];
+		scanf("%s",word);
+		int j=0;
+		while(word[j]!=NULL){
+			for(int i=0;i<num;i++){
+				printf("%c",word[j]);
+			}
+			j++;
+		}
+		printf("\n");
+	}
+	return 0;
+}
