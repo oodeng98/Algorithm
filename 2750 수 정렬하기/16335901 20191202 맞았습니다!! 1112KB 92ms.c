b47d900942c63f2fa6ec7cb2c586785ca5253415
oodeng98 <52957193+oodeng98@users.noreply.github.com>
2021-03-06 16:31:51 +0900

    풀이 과정
    git 사용 전이라 풀이과정 생략

---
+++ 2750 수 정렬하기/16335901 20191202 맞았습니다!! 1112KB 92ms.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+	int number;
+	scanf("%d",&number);
+	int numbers[1000];
+	for(int i=0;i<number;i++){
+		scanf("%d",&numbers[i]);
+	}
+	int temp;
+	for(int i=1;i<number;i++){
+		if(numbers[i-1]>numbers[i]){
+			temp = numbers[i-1];
+			numbers[i-1] = numbers[i];
+			numbers[i] = temp;
+			i = 0;
+			continue;
+		}
+	}
+	for(int i=0;i<number;i++){
+		printf("%d\n",numbers[i]);
+	}
+	return 0;
+}
