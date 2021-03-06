fa1b1a1bcce810da455f41525a120cf1ddbf83a1
oodeng98 <52957193+oodeng98@users.noreply.github.com>
2021-03-06 21:02:28 +0900

    풀이 과정
    git 사용 전이라 풀이과정 생략

---
+++ 2753 윤년/16347821 20191203 맞았습니다!! 1112KB 0ms.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int a;
+	scanf("%d",&a);
+	if((a%4==0)&&(a%100!=0)){
+		printf("1");
+	}
+	else if((a%4==0)&&(a%400==0)){
+		printf("1");
+	}
+	else{
+		printf("0");
+	}
+	return 0;
+}
