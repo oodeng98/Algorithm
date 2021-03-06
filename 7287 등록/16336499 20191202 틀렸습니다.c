3bf06ee9027abe94ba4be339eccbf31714d51298
oodeng98 <52957193+oodeng98@users.noreply.github.com>
2021-03-06 16:39:40 +0900

    풀이 과정
    git 사용 전이라 풀이과정 생략

---
+++ 7287 등록/16336499 20191202 틀렸습니다.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	printf("123\n");
+	printf("Your_ICPC_Team_Name");
+	return 0;
+}
---
+++ 7287 등록/16336509 20191202 맞았습니다!! 1112KB 0ms.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	printf("8\n");
+	printf("parenhark");
+	return 0;
+}
