26e7597691ae8e3bcac5c5725b2973bb9d31fc03
oodeng98 <52957193+oodeng98@users.noreply.github.com>
2021-03-07 20:43:55 +0900

    풀이 과정
    git 사용 전이라 풀이과정 생략

---
+++ 1012 유기농 배추/26690045 20210224 맞았습니다!! 29284KB 3812ms.py
@@ -0,0 +1,42 @@
+import sys
+
+
+def ver(a, b):
+    if a[0] == b[0]:
+        if a[1] - b[1] == 1 or b[1] - a[1] == 1:
+            return 1
+    return 0
+
+
+def hor(a, b):
+    if a[1] == b[1]:
+        if a[0] - b[0] == 1 or b[0] - a[0] == 1:
+            return 1
+    return 0
+
+
+input = sys.stdin.readline
+num = int(input())
+for k in range(num):
+    farm = list(map(int, input().split()))
+    data = []
+    for i in range(farm[-1]):
+        data.append(list(map(int, input().split())))
+    group = []
+    while True:
+        if len(data) == 0:
+            print(len(group))
+            break
+        temp = [data.pop(0)]
+        i = 0
+        while True:
+            for j in data:
+                if ver(temp[i], j) or hor(temp[i], j):
+                    if j not in temp:
+                        temp.append(j)
+            i += 1
+            if i >= len(temp):
+                break
+        for i in range(1, len(temp)):
+            data.remove(temp[i])
+        group.append(temp)
