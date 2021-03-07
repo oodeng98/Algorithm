399ffe3d76265adc72e048b10305152dc105ef92
oodeng98 <52957193+oodeng98@users.noreply.github.com>
2021-03-07 20:53:49 +0900

    풀이 과정
    git 사용 전이라 풀이과정 생략

---
+++ 18870 좌표 압축/26855919 20210301 맞았습니다!! 154448KB 2236ms.py
@@ -0,0 +1,12 @@
+import sys
+
+
+input = sys.stdin.readline
+num = int(input())
+data = list(map(int, input().split()))
+ele = sorted(list(set(data)))
+data_dict = {}
+for i in range(len(ele)):
+    data_dict[ele[i]] = i
+for i in data:
+    print(data_dict[i], end=' ')
