cf0c63f7a664c003901ee334918736904a0d0397
oodeng98 <52957193+oodeng98@users.noreply.github.com>
2021-03-07 11:26:01 +0900

    풀이 과정
    git 사용 전이라 풀이과정 생략

---
+++ 1929 소수 구하기/16474664 20191215 시간 초과.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+	int low,high;
+	scanf("%d",&low);
+	scanf("%d",&high);
+	if(low==1){
+		low = 2;
+	}
+	for(int i=low;i<=high;i++){
+		int torf = 0;
+		for(int j=2;j<=i/2;j++){
+			if(i%j==0){
+				torf = 1;
+				break;
+			}
+		}
+		if(torf==0){
+			printf("%d\n",i);
+		}
+	}
+	return 0;
+}
