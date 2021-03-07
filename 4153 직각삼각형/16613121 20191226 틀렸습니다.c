a7ce766dbbf4307c7cbc96d547962c52e023edf1
oodeng98 <52957193+oodeng98@users.noreply.github.com>
2021-03-07 11:31:03 +0900

    풀이 과정
    git 사용 전이라 풀이과정 생략

---
+++ 4153 직각삼각형/16613121 20191226 틀렸습니다.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int findbiggest(int a, int b, int c){
+	if(a>=b&&a>=c){
+		return a;
+	}
+	else if(b>=c&&b>=a){
+		return b;
+	}
+	else{
+		return c;
+	}
+}
+int main(){
+	int a,b,c;
+	while(1){
+		scanf("%d",&a);
+		scanf("%d",&b);
+		scanf("%d",&c);
+		if(a==0&&b==0&&c==0){
+			break;
+		}
+		int big = findbiggest(a,b,c);
+		if(big*big*2==a*a+b*b+c*c){
+			printf("right");
+		}
+		else{
+			printf("wrong");
+		}
+
+	}
+	return 0;
+}
