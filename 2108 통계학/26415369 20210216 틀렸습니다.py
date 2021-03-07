bd6a5d3e4ab2df73b6124594af73dcbd0c5c747d
oodeng98 <52957193+oodeng98@users.noreply.github.com>
2021-03-07 15:57:07 +0900

    풀이 과정
    git 사용 전이라 풀이과정 생략

---
+++ 2108 통계학/26415369 20210216 틀렸습니다.py
@@ -0,0 +1,31 @@
+num = int(input())
+data = []
+for i in range(num):
+    data.append(int(input()))
+
+san = 0
+count = {}
+for i in data:
+    if i in count.keys():
+        count[i] += 1
+    else:
+        count[i] = 1
+    san += i
+san = san / num
+if int(san) * 2 == int(san * 2):
+    print(int(san))
+else:
+    print(int(san+1))
+
+data_sorted = sorted(data)
+print(data_sorted[num//2])
+
+count = [(x, y) for x, y in count.items()]
+count = sorted(count, key=lambda x: x[1], reverse=True)
+if count[0][1] == count[1][1]:
+    count = sorted(count, key=lambda x: x[0])
+    print(count[1][0])
+else:
+    print(count[0][0])
+
+print(data_sorted[-1] - data_sorted[0])
