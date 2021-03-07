e5c617336644c7291079f1096fca250469ef2fa1
oodeng98 <52957193+oodeng98@users.noreply.github.com>
2021-03-07 15:24:21 +0900

    풀이 과정
    git 사용 전이라 풀이과정 생략

---
+++ 1654 랜선 자르기/26293693 20210212 틀렸습니다.py
@@ -0,0 +1,36 @@
+def find_best(start, finish, lst, need):
+    while True:
+        # print(start, finish)
+        if finish - start <= 1:
+            last_check = 0
+            for i in lst:
+                last_check += i // finish
+                if last_check == need:
+                    return finish
+            break
+        middle = (start + finish) // 2
+        check = 0
+        for i in lst:
+            check += i // middle
+        if check >= need:
+            start = middle
+        else:
+            finish = middle
+
+    return start
+
+
+nums = input().split(' ')
+num = int(nums[0])
+need = int(nums[1])
+data = []
+for i in range(num):
+    d = int(input())
+    data.append(d)
+
+total = 0
+for i in data:
+    total += i
+best = total // need
+
+print(int(find_best(1, best, data, need)))
