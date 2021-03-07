785a9abe31874ecf98b66ee34c1706f223d7b752
oodeng98 <52957193+oodeng98@users.noreply.github.com>
2021-03-07 15:57:13 +0900

    풀이 과정
    git 사용 전이라 풀이과정 생략

---
+++ 9012 괄호/26381516 20210215 틀렸습니다.py
@@ -0,0 +1,19 @@
+num = int(input())
+for i in range(num):
+    data = input()
+    # print(data)
+    stack = 0
+    check = 0
+    for j in range(len(data)-1):
+        if data[j] == '(':
+            stack += 1
+        else:
+            stack -= 1
+            if stack < 0:
+                print('NO')
+                check = 1
+                break
+    if check != 1 and stack == 1 and data[-1] == ')':
+        print('YES')
+    else:
+        print('NO')
