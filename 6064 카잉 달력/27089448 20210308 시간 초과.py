c617b3c98737ee18749c6fdc22f27674f8b2ff10
oodeng98 <52957193+oodeng98@users.noreply.github.com>
2021-03-08 21:35:33 +0900

    풀이 과정
    m이 분모로 가지 않는 오류를 수정해줬으나 시간 초과는 해결하지 못함

---
+++ 6064 카잉 달력/27089448 20210308 시간 초과.py
@@ -0,0 +1,57 @@
+import sys
+
+
+input = sys.stdin.readline
+num = int(input())
+
+for i in range(num):
+    m, n, x, y = map(int, input().split())
+    if m > n:  # n을 더 큰 값으로 둔다
+        m, n = n, m
+        x, y = y, x
+    if (x - y) % 2 == 1 and m % 2 == 0:
+        print(-1)
+    elif m == n and x != y:
+        print(-1)
+    else:
+        result = y - x
+        while result % m != 0:
+            result += n
+        print(result + x)
+# result % m == x
+# result % n == y
+
+"""
+15
+40000 39999 39999 39998
+40000 39999 40000 39999
+40000 40000 40000 39999
+40000 39998 40000 39997
+39999 2 39998 2
+3 40000 3 39999
+40000 3 40000 3
+8 2 4 2
+10 12 2 12
+12 10 12 10
+12 10 1 1
+12 6 12 6
+10 1 5 1
+1 10 1 5
+1 1 1 1
+
+1599959999
+1599960000
+-1
+-1
+39998
+39999
+120000
+4
+12
+60
+1
+12
+5
+5
+1
+"""
