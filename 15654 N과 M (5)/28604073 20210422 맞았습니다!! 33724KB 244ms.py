c7a9e5d705d2a06e7b816ae1688c126d9e73ae21
oodeng98 <52957193+oodeng98@users.noreply.github.com>
2021-04-22 12:12:13 +0900

    풀이 과정
    마찬가지로 itertools를 사용해서 굉장히 쉽게 풀었다.

---
+++ 15654 N과 M (5)/28604073 20210422 맞았습니다!! 33724KB 244ms.py
@@ -0,0 +1,15 @@
+import sys
+import itertools
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+n_list = sorted(list(map(int, input().split())))
+if m == 1:
+    for i in n_list:
+        print(i)
+else:
+    result = list(itertools.permutations(n_list, m))
+    for i in result:
+        for j in i:
+            print(j, end=' ')
+        print()
