0ea5c465abe928229cebd9a8baefdb20ebf00d62
oodeng98 <52957193+oodeng98@users.noreply.github.com>
2021-04-29 14:05:34 +0900

    풀이 과정
    처음에 start=50을 설정하고 제출해서 런타임 에러가 발생한 것이다.

---
+++ 5639 이진 검색 트리/28813188 20210429 런타임 에러 (KeyError).py
@@ -0,0 +1,42 @@
+import sys
+
+
+def postorder(trees, now):
+    if now == -1:
+        return
+    postorder(trees, trees[now][0])
+    postorder(trees, trees[now][1])
+    print(now)
+
+
+input = sys.stdin.readline
+data = []
+while True:
+    try:
+        data.append(int(input()))
+    except ValueError:
+        break
+index = 2
+tree = {data[0]: [-1, -1]}
+if data[0] < data[1]:
+    tree[data[0]][1] = data[1]
+else:
+    tree[data[0]][0] = data[1]
+tree[data[1]] = [-1, -1]
+
+for index in range(2, len(data)):
+    start = data[0]
+    while True:  # 자신과 자신의 부모 노드 사이에 있으면 오른쪽에 추가, 그게 아니면 왼쪽에 추가
+        if start < data[index]:
+            new_start = tree[start][1]
+        else:  # data[index] < start
+            new_start = tree[start][0]
+        if new_start == -1:
+            if start < data[index]:
+                tree[start][1] = data[index]
+            else:
+                tree[start][0] = data[index]
+            tree[data[index]] = [-1, -1]
+            break
+        start = new_start
+postorder(tree, data[0])
