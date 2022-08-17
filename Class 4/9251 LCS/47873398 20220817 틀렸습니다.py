fd443a4686f53055a62e10a7707669451514e0ca
oodeng98 <oodeng98@naver.com>
2022-08-17 19:17:47 +0900

    풀이 과정 다이나믹 프로그래밍 문제들은 항상 범위를 점점 넓혀가는 것에 초점을 두는 듯 싶다. 이번 문제도 마찬가지로, 늘어난 하나의 알파벳이 LCS에 포함되느냐, 되지 않느냐로 천천히 문제를 해결하는 방식이다. 다음에도 다이나믹 프로그래밍을 마주친다면, 일단 2차원 배열을 만들어놓고 일부분을 수작업으로 해결하여 규칙을 찾는 방식을 도입해보자.

--- Class 4/9251 LCS/47873398 20220817 틀렸습니다.py
+++ Class 4/9251 LCS/47873398 20220817 틀렸습니다.py
@@ -1,8 +1,15 @@
 import sys
 
 input = sys.stdin.readline
-str1 = input().strip()
-str2 = input().strip()
+temp1 = input().strip()
+temp2 = input().strip()
+
+if len(temp1) < len(temp2):
+    str1 = temp1
+    str2 = temp2
+else:
+    str1 = temp2
+    str2 = temp1
 
 # 첫번째 문장의 알파벳들이 두번째 문장에서 각각 몇번째에 위치하는지 적어놓고
 # 그 숫자들을 아래에서부터 가장 긴 수열로 만들어주면?
---
+++ Class 4/9251 LCS/47874120 20220817 맞았습니다!! 55844KB 696ms.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+str1 = input().strip()
+str2 = input().strip()
+
+dp = []
+for i in range(len(str1) + 1):
+    dp.append([0 for x in range(len(str2) + 1)])
+
+for i in range(1, len(str1) + 1):
+    for j in range(1, len(str2) + 1):
+        if str1[i - 1] == str2[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+print(dp[len(str1)][len(str2)])
