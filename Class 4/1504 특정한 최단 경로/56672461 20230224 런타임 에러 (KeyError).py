5573b876cfb61980e60d48aa913b8f07c7bfdac0
oodeng98 <oodeng98@naver.com>
2023-03-01 21:43:14 +0900

    다익스트라 알고리즘을 3번 적용함으로써 풀 수 있었다. 상위 답안들을 확인해보니 비슷한 방식으로 푼 것을 확인할 수 있었다.

---
+++ Class 4/1504 특정한 최단 경로/56672461 20230224 런타임 에러 (KeyError).py
@@ -0,0 +1,57 @@
+import sys
+import heapq
+
+
+def search(graph, start):
+    distances = {i: float('inf') for i in range(1, n + 1)}
+    distances[start] = 0
+    queue = []
+    heapq.heappush(queue, [distances[start], start])
+
+    while queue:
+        current_distance, current_destination = heapq.heappop(queue)
+
+        if distances[current_destination] < current_distance:
+            continue
+
+        for new_destination, new_distance in graph[current_destination].items():
+            distance = current_distance + new_distance
+            if distance < distances[new_destination]:
+                distances[new_destination] = distance
+                heapq.heappush(queue, [distance, new_destination])
+    return distances
+
+
+input = sys.stdin.readline
+n, e = map(int, input().split())
+graph = {}
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    if a in graph:
+        if b in graph[a]:
+            graph[a][b] = min(graph[a][b], c)
+        else:
+            graph[a][b] = c
+    else:
+        graph[a] = {b: c}
+    if b in graph:
+        if a in graph[b]:
+            graph[b][a] = min(graph[b][a], c)
+        else:
+            graph[b][a] = c
+    else:
+        graph[b] = {a: c}
+via1, via2 = map(int, input().split())
+
+from_start = search(graph, 1)
+from_via1 = search(graph, via1)
+from_via2 = search(graph, via2)
+ret1 = from_start[via1] + from_via1[via2] + from_via2[n]
+ret2 = from_start[via2] + from_via2[via1] + from_via1[n]
+if ret1 == float('inf'):
+    if ret2 == float('inf'):
+        print(-1)
+    else:
+        print(ret2)
+else:
+    print(min(ret1, ret2))
