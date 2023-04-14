2878a6104cd37fd706ee7e1cfc694db9a0ffb42a
oodeng98 <oodeng98@naver.com>
2023-04-14 23:23:36 +0900

    경유지를 한꺼번에 저장한다는 생각이 패착이다, 경유지를 가기 위한 경유지를 구하는 과정을 출발지가 나올 때 까지 진행하면 되는 것이다.

---
+++ Class 4/11779 최소비용 구하기 2/59383902 20230414 런타임 에러 (KeyError).py
@@ -0,0 +1,51 @@
+import sys
+import heapq
+
+
+def search(graph, start):
+    distances = {i: [float('inf'), 0] for i in range(1, n + 1)}
+    distances[start] = [0, 0]
+    queue = []
+    heapq.heappush(queue, [distances[start][0], start])
+
+    while queue:
+        current_distance, current_destination = heapq.heappop(queue)
+
+        if distances[current_destination][0] < current_distance:
+            continue
+
+        if current_destination in graph:
+            for new_destination, new_distance in graph[current_destination].items():
+                distance = current_distance + new_distance
+                if distance < distances[new_destination][0]:
+                    distances[new_destination] = [distance, current_destination]
+                    heapq.heappush(queue, [distance, new_destination])
+    return distances
+
+
+input = sys.stdin.readline
+n = int(input())
+m = int(input())
+graph = {}
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    if a in graph:
+        if b in graph:
+            graph[a][b] = min(graph[a][b], c)
+        else:
+            graph[a][b] = c
+    else:
+        graph[a] = {b: c}
+start, des = map(int, input().split())
+result = search(graph, start)
+
+print(result[des][0])
+ret = []
+via = result[des][1]
+while via != 0:
+    ret.append(str(via))
+    via = result[via][1]
+
+print(len(ret))
+ret = reversed(ret)
+print(' '.join(ret))
