cbc5b4dbce943019965b250ead808975245a082c
oodeng98 <oodeng98@naver.com>
2023-11-02 16:46:09 +0900

    fix: 출력 형식 변경
    출력의 맨 앞에 \n이 붙어서 틀린 것이라고 추정된다.

--- Class 5/1197 최소 스패닝 트리/20230414.py
+++ Class 5/1197 최소 스패닝 트리/20230414.py
@@ -8,5 +8,18 @@ queue = []
 for _ in range(e):
     a, b, c = map(int, input().split())
     heapq.heappush(queue, [c, a, b])
+
+check = [i for i in range(v + 1)]
 while queue:
-    print(heapq.heappop(queue))
+    temp = heapq.heappop(queue)
+    if check[temp[1]] == temp[1] and check[temp[2]] == temp[2]:  # 두개의 노드가 어느 그룹에도 속해있지 않은 경우
+        mini = min(temp[1], temp[2])
+        check[temp[1]] = mini
+        check[temp[2]] = mini
+    elif check[temp[2]] == temp[2]:  # temp[1]만 그룹에 속해있는 경우
+        check[temp[2]] = check[temp[1]]
+    elif check[temp[1]] == temp[1]:  # temp[2]만 그룹에 속해있는 경우
+        check[temp[1]] = check[temp[2]]
+
+
+
