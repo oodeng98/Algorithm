8b3146db05f8acf2f7b4f818aac7b22052152d02
unknown <oodeng98@naver.com>
2024-02-05 14:18:51 +0900

    feat: 행렬의 덧셈 문제 풀이

---
+++ Programmers/Level1/행렬의 덧셈/240205.py
@@ -0,0 +1,8 @@
+def solution(arr1, arr2):
+    answer = []
+    for i in range(len(arr1)):
+        temp = []
+        for j in range(len(arr1[0])):
+            temp.append(arr1[i][j] + arr2[i][j])
+        answer.append(temp)
+    return answer
