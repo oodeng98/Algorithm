7a4d03bda37cb9457e5ce728ea0720c10dc110ef
unknown <oodeng98@naver.com>
2024-02-13 12:44:20 +0900

    feat: 4615 재미있는 오셀로 게임 문제 풀이
    사이에 존재하는 돌이 하나가 아니어도 된다는 사실이 중요

---
+++ SW Expert Academy/D3/4615. 재미있는 오셀로 게임/240213.py
@@ -0,0 +1,41 @@
+import sys
+
+sys.stdin = open('input.txt')
+
+def delta(x, y, color):
+    board[x][y] = color
+    for i, j in [[-1, 0], [1, 0], [0, -1], [0, 1], [1, 1], [-1, -1], [1, -1], [-1, 1]]:
+        count = 1
+        while 0 <= x + i * count < N and 0 <= y + j * count < N:
+            if board[x+i*count][y+j*count] == 0:
+                break
+            elif board[x+i*count][y+j*count] == color:
+                for k in range(1, count):
+                    board[x+i*k][y+j*k] = color
+                break
+            count += 1
+
+
+T = int(input())
+for t in range(1, T+1):
+    N, M = map(int, input().split())
+
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    for i in range(N//2-1, N//2+1):
+        board[i][i] = 2
+        board[i][N-i-1] = 1
+    
+    seq = []
+    for _ in range(M):
+        y, x, color = map(int, input().split())  # y, x, color: 1은 흑돌, 2는 백돌
+        delta(x-1, y-1, color)
+    
+    black = 0
+    white = 0
+    for i in board:
+        for j in i:
+            if j == 1:
+                black += 1
+            elif j == 2:
+                white += 1
+    print(f"#{t} {black} {white}")
