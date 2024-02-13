8cd2de7fcd91e7add1a12217f556128549930676
unknown <oodeng98@naver.com>
2024-02-13 10:19:44 +0900

    feat: 5432 쇠막대기 자르기 문제 풀이

---
+++ SW Expert Academy/D4/5432. 쇠막대기 자르기/240213.py
@@ -0,0 +1,23 @@
+import sys
+
+sys.stdin = open('input.txt')
+for t in range(1, int(input()) + 1):
+    stack = []
+    result = 0
+    count = 0
+    batch = input()
+    i = 0
+    length = len(batch)
+    while i < length:
+        if batch[i] == '(':
+            if batch[i+1] == ')':
+                result += count
+                i += 2
+                continue
+            else:
+                count += 1
+        else:
+            count -= 1
+            result += 1
+        i += 1
+    print(f"#{t} {result}")
