8d5b66575dae763937d6057f845692a716188453
unknown <oodeng98@naver.com>
2024-02-13 17:51:08 +0900

    feat: 1222 계산기1 문제 풀이
    문제의 목적에 맞게 다시 풀이

--- SW Expert Academy/D4/1222. [SW 문제해결 기본] 6일차 - 계산기1/240213.py
+++ SW Expert Academy/D4/1222. [SW 문제해결 기본] 6일차 - 계산기1/240213.py
@@ -4,7 +4,20 @@ sys.stdin = open('input.txt')
 
 for t in range(1, 11):
     N = int(input())
-    result = 0
-    for i in input().split('+'):
-        result += int(i)
-    print(f"#{t} {result}")
+    stack = []
+    result = ''
+    operator = ''
+    for i in input().strip():
+        if i.isdecimal():
+            result += i
+            continue
+        if operator:
+            result += i
+        else:
+            operator = i
+    result += operator
+    ret = 0
+    for i in result[::-1]:
+        if i.isdecimal():
+            ret += int(i)
+    print(f"#{t} {ret}")
