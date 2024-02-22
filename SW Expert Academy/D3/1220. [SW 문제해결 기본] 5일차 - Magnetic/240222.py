c4b28035a63b978ef449b82fb939d64510fb5e3a
unknown <oodeng98@naver.com>
2024-02-22 13:59:31 +0900

    feat: 1220 Magnetic 문제 재풀이
    쓸모없는 코드를 줄이고 수행 과정을 간략하게 변경

--- SW Expert Academy/D3/1220. [SW 문제해결 기본] 5일차 - Magnetic/240222.py
+++ SW Expert Academy/D3/1220. [SW 문제해결 기본] 5일차 - Magnetic/240222.py
@@ -7,26 +7,20 @@ for t in range(1, 11):
     N = int(input())
     table = [input().split() for _ in range(N)]
     # 1은 N, 2는 S
-    new_table = []
+    result = 0
     for i in range(N):
         temp = ''
         for j in range(N):
             if table[j][i] != '0':
                 temp += table[j][i]
-        new_table.append(temp)
 
-    result = 0
-    for i in range(N):
-        temp = new_table.pop()
         temp = temp.lstrip('2')
         temp = temp.rstrip('1')
         l = '1'
         r = '2'
         while temp:
             temp = temp.lstrip(l)
-            temp = temp.lstrip('0')
             temp = temp.rstrip(r)
-            temp = temp.rstrip('0')
             result += 1
             l, r = r, l
     print(f"#{t} {result}")
