65923b512ffdb58a96e2f550b1540ad054042df6
JoungTaeWan <oodeng98@naver.com>
2024-02-28 10:00:59 +0900

    feat: 4012 요리사 문제 풀이

--- SW Expert Academy/모의 SW 역량테스트/4012. 요리사/240227.py
+++ SW Expert Academy/모의 SW 역량테스트/4012. 요리사/240227.py
@@ -10,9 +10,18 @@ T = int(input())
 for t in range(1, T+1):
     N = int(input())
     synergy = [list(map(int, input().split())) for _ in range(N)]
-    lst = [i for i in range(N//2)]
-    for comb in combinations(lst, N//2):
-        
-        print(i)
-    print(f"#{t} {0}")
-    break
+    lst = list(combinations([i for i in range(N)], N//2))
+    result = float('inf')
+    for i in range(len(lst) // 2):
+        food1 = lst[i]
+        food2 = lst[-(i+1)]
+        score1 = 0
+        score2 = 0
+        for i in range(N//2):
+            for j in range(i+1, N//2):
+                a1, b1 = food1[i], food1[j]
+                a2, b2 = food2[i], food2[j]
+                score1 += synergy[a1][b1] + synergy[b1][a1]
+                score2 += synergy[a2][b2] + synergy[b2][a2]
+        result = min(result, abs(score1 - score2))
+    print(f"#{t} {result}")
