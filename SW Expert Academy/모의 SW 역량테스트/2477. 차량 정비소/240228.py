a7ae50ed3197ac5c57826e80bad2390dab2eadaf
JoungTaeWan <oodeng98@naver.com>
2024-02-29 10:37:52 +0900

    feat: 2477 차량 정비소 문제 재풀이
    변수 통합 및 쓸모없는 변수 제거

--- SW Expert Academy/모의 SW 역량테스트/2477. 차량 정비소/240228.py
+++ SW Expert Academy/모의 SW 역량테스트/2477. 차량 정비소/240228.py
@@ -8,7 +8,7 @@ for t in range(1, T+1):
     N, M, K, A, B = map(int, input().split())
     reception_time = list(map(int, input().split()))
     repair_time = list(map(int, input().split()))
-    clients = list(map(int, input().split()))
+    clients = map(int, input().split())
     
     reception_list = [[] for _ in range(N)]  # 손님 이름 저장용
     reception = [0 for _ in range(N)]  # 접수가 끝나는 시간 저장
@@ -16,7 +16,6 @@ for t in range(1, T+1):
 
     for index, client in enumerate(clients):
         min_value = float('inf')
-        min_index = -1
         reception_num = -1
         for i in range(N):
             if reception[i] <= client:
@@ -25,33 +24,29 @@ for t in range(1, T+1):
                 break
             if reception[i] < min_value:
                 min_value = reception[i]
-                min_index = i
+                reception_num = i
         else:   
-            reception[min_index] += reception_time[min_index]
-            reception_num = min_index
+            reception[reception_num] += reception_time[reception_num]
         reception_list[reception_num].append(index+1)
         repair_clients.append((index+1, reception[reception_num], reception_num))
 
     repair_clients.sort(key=lambda x: (x[1], x[2]))
     repair_list = [[] for _ in range(N)]  # 손님 이름 저장용
-    repair = [0 for _ in range(N)]  # 접수가 끝나는 시간 저장
+    repair = [0 for _ in range(N)]  # 정비가 끝나는 시간 저장
 
     for index, client, _ in repair_clients:
         min_value = float('inf')
-        min_index = -1
         repair_num = -1
         for i in range(M):
             if repair[i] <= client:
                 repair[i] = client + repair_time[i]
                 repair_num = i
-                repair_list[i].append(index)
                 break
             if repair[i] < min_value:
                 min_value = repair[i]
-                min_index = i
+                repair_num = i
         else:    
-            repair[min_index] += repair_time[min_index]
-            repair_num = min_index
+            repair[repair_num] += repair_time[repair_num]
         repair_list[repair_num].append(index)
 
     result = sum(set(reception_list[A-1]) & set(repair_list[B-1]))
