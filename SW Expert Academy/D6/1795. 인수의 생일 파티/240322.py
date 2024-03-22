38add2de9f147c82c8983593828946a5b442f114
JoungTaeWan <oodeng98@naver.com>
2024-03-22 12:48:26 +0900

    feat: 1795 인수의 생일 파티 문제 재풀이
    bellman ford 알고리즘을 사용해서 풀이

--- SW Expert Academy/D6/1795. 인수의 생일 파티/240322.py
+++ SW Expert Academy/D6/1795. 인수의 생일 파티/240322.py
@@ -22,6 +22,19 @@ def dijkstra(start, end):
     return distance[end]
 
 
+def bellman_ford(start):
+    distance = [float('inf') for _ in range(N+1)]
+    distance[start] = 0
+    for _ in range(N-1):
+        for i in graph:
+            for j in graph[i]:
+                cur_node, next_node, weight = i, j, graph[i][j]
+                new_dist = distance[cur_node] + weight
+                if distance[cur_node] != float('inf') and new_dist < distance[next_node]:
+                    distance[next_node] = new_dist
+    return distance
+
+
 sys.stdin = open('input.txt')
 T = int(input())
 for t in range(1, T+1):
@@ -33,10 +46,10 @@ for t in range(1, T+1):
             graph[x][y] = c
         else:
             graph[x] = {y: c}
-    
+    bell_dist = bellman_ford(X)
     result = 0
     for node in range(1, N+1):
         if node == X:
             continue
-        result = max(result, dijkstra(X, node) + dijkstra(node, X))
+        result = max(result, bell_dist[node] + dijkstra(node, X))
     print(f"#{t} {result}")
