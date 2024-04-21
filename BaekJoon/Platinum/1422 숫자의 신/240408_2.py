a5a038ee497fea41afc900efb1503b5cf6da58b0
oodeng98 <oodeng98@naver.com>
2024-04-21 10:48:05 +0900

    feat: 1422 숫자의 신 문제 풀이
    str의 특징을 이용해서 길이를 맞춰주고 해당 값들을 비교하면
    숫자의 크기를 명확하게 비교할 수 있음을 알아야 풀이할 수 있었음

--- BaekJoon/Platinum/1422 숫자의 신/240408_2.py
+++ BaekJoon/Platinum/1422 숫자의 신/240408_2.py
@@ -1,41 +1,13 @@
 import sys
 
 
-def dfs(n, num):
-    print(n, num)
-    if n <= 0:
-        global result
-        result = max(result, int(num))
-        return
-    if nums[str(n)]:
-        for i in nums[str(n)]:
-            if i == max_value:
-                pass
-            dfs(n - 1, num + i)  # 빼고 다시 돌아야할거같은딩..?
-    else:
-        dfs(n - 1, num)
-
-
 input = sys.stdin.readline
-K, N = map(int, input().split())
-nums = {str(i): [] for i in range(1, 10)}
-for _ in range(K):
-    temp = input().strip()
-    nums[temp[0]].append(temp)
+k, n = map(int,input().split())
+lst = [input().strip() for _ in range(k)]
 
-# 1. 나중에 더해줄 숫자 구하기
-max_value = max(nums, key=lambda x: int(x))
-
-# 2. 지금 있는 숫자들 순서 정하기
-result = 0
-dfs(9, '')
-print(result)
-'''
-7
-6
-754
-이렇게 나오면
-현재 코드에서는 75476으로 표기하지만
-실제로는 76754로 가는게 맞다
-흐음... 이걸 어떻게 처리하지
-'''
+# 길이가 길고 그중에서 숫자가 가장 큰 애가 앞으로 오게끔 정렬
+lst.sort(key =lambda x:(-len(x),-int(x)))
+num = lst + [lst[0]] * (n-k)
+length = len(lst[0])
+num.sort(key=lambda x: x*length, reverse=True)  # 최소 길이를 맞춰주고 비교
+print(''.join(num))
