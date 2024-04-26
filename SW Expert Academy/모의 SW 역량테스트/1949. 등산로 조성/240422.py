a0f78d2b3e77a728af2b592185eb564665004054
JoungTaeWan <oodeng98@naver.com>
2024-04-26 13:54:51 +0900

    docs: 1949 등산로 조정 중간 저장

---
+++ SW Expert Academy/모의 SW 역량테스트/1949. 등산로 조성/240422.py
@@ -0,0 +1,46 @@
+import sys
+
+sys.stdin = open('input.txt')
+
+
+def delta(x, y):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    for i in range(4):
+        a = x + dx[i]
+        b = y + dy[i]
+        if 0 <= a < N and 0 <= b < N:
+            yield a, b
+
+
+def dfs(x, y, k, count):
+    check = 0
+    for a, b in delta(x, y):
+        if arr[a][b] < arr[x][y]:
+            check = 1
+            dfs(x, y, k, count+1)
+        elif arr[a][b] < arr[x][y] + k:
+            check = 1
+            temp = arr[a][b]
+            arr[a][b] = arr[x][y] - 1
+            dfs(a, b, 0, count+1)
+            arr[a][b] = temp
+    if not check:  # 더이상 갈 곳이 없으면?
+        global max_length
+        max_length = max(max_length, count)
+
+
+T = int(input())
+for t in range(1, T+1):
+    N, K = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    max_length = 1
+    max_value = 0
+    for i in arr:
+        for j in i:
+            max_value = max(max_value, j)
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] == max_value:
+                dfs(i, j, K, 1)
+    print(f"#{t} {max_value}")
