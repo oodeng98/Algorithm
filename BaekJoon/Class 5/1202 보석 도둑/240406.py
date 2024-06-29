c46dd2e36f18c74732527b80da50f39b9e71e8e4
oodeng98 <oodeng98@naver.com>
2024-06-29 15:34:12 +0900

    feat: 1202 보석 도둑 주석 수정

--- BaekJoon/Class 5/1202 보석 도둑/240406.py
+++ BaekJoon/Class 5/1202 보석 도둑/240406.py
@@ -11,18 +11,21 @@ for _ in range(N):  # 반복문 N번을 도는 것은 동일하지만, list comp
         jewels[weight].append(value)
     else:
         jewels[weight] = [value]
-jewels_weight = sorted(jewels.keys(), reverse=True)  # 가방의 개수를 줄였으므로 여기서 nlogn의 시간복잡도가 조금 줄었을 수 있음
+jewels_weight = sorted(jewels.keys(), reverse=True)  # 내림차순으로 보석 정렬
         
 bags = sorted([int(input()) for _ in range(K)])
 
 result = 0
 heap = []
-for bag in bags:
+for bag in bags:  # 가방 무게별로 반복 시행
     while jewels_weight and jewels_weight[-1] <= bag:  # 어차피 도는 반복문의 횟수는 똑같음
         weight = jewels_weight.pop()
         for value in jewels[weight]:  # 앞서 아낀 부분도 여기서 추가로 돌기 때문
-            heappush(heap, -value)
+            heappush(heap, -value)  # heap에는 가방에 넣을 수 있는 모든 보석의 가치가 들어감
     if heap:
         result += heappop(heap)
 print(-result)
 # 그런데 왜 시간이 훨씬 빠른가..?
+'''
+https://www.acmicpc.net/problem/1202
+'''
